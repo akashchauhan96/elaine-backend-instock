fceb83e13fb8c2e1526d95ef95a279691f55dd48
Akash Chauhan <akash.sc96@gmail.com>
2022-11-21 17:57:00 -0500

    Initial Folder Structure & Git Repo

---
+++ index.js
@@ -0,0 +1,20 @@
+require("dotenv").config();
+
+const express = require("express");
+const app = express();
+const fs = require("fs");
+const cors = require("cors");
+const warehouseRoute = require("./routes/warehouse.js");
+const inventoryRoute = require("./routes/inventory.js");
+
+const PORT = process.env.PORT;
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/warehouse", warehouseRoute);
+app.use("/inventory", inventoryRoute);
+
+app.listen(PORT, () => {
+  console.log("HELLO DR.NASRI");
+});
