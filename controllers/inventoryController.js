1d0588370361d622323dc1ff862383e26971bec9
Troy-Smith-95 <trsmith.runner@gmail.com>
2022-11-24 22:27:33 -0500

    add join of inventories and warehouses tables for getAll invetories and select specific columns to return

--- controllers/inventoryController.js
+++ controllers/inventoryController.js
@@ -2,6 +2,8 @@ const knex = require("knex")(require("../knexfile"));
 
 const getAll = (_req, res) => {
     knex("inventories")
+    .join('warehouses', 'inventories.warehouse_id', 'warehouses.id')
+    .select('inventories.id', 'warehouses.warehouse_name', 'inventories.item_name', 'inventories.description', 'inventories.category', 'inventories.status', 'inventories.quantity')
     .then((data) => {
       res.status(200).json(data);
     })
