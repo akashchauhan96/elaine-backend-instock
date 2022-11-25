4118fb90b512091fea6e385f60b49ba4fd2336ef
Akash Chauhan <akash.sc96@gmail.com>
2022-11-24 20:36:44 -0500

    Complete merging with develop into feature branch and resolve conflicts

--- routes/warehouse.js
+++ routes/warehouse.js
@@ -4,7 +4,6 @@ const warehouseController = require("../controllers/warehouseController");
 
 router.route("/").get(warehouseController.getAll);
 
-router.route("/:id").get(warehouseController.getOne);
 router
   .route("/:id")
   .get(warehouseController.getOne)
