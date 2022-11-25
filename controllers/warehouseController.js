54d64de237d4fa42995bf032615d3f9eee8f9668
Akash Chauhan <akash.sc96@gmail.com>
2022-11-24 20:29:39 -0500

    Git pull with most recent backend code

--- controllers/warehouseController.js
+++ controllers/warehouseController.js
@@ -1,41 +1,40 @@
-const knex = require('knex')(require('../knexfile'));
+const knex = require("knex")(require("../knexfile"));
 
 // GET all of the warehouses within the warehouses table in instock database
 const getAll = (_req, res) => {
-    knex('warehouses')
+  knex("warehouses")
     .then((data) => {
-        res.status(200).json(data);
-     })
+      res.status(200).json(data);
+    })
     .catch((err) => {
-        res.status(400).send(`Error retrieving warehouses ${err}`);    
+      res.status(400).send(`Error retrieving warehouses ${err}`);
     });
-}
+};
 
 const getOne = (req, res) => {
-    knex('warehouses')
+  knex("warehouses")
     .where({ id: req.params.id })
     .then((data) => {
-        res.status(200).json(data[0]);
+      res.status(200).json(data[0]);
     })
     .catch((err) => {
-        res.status(400).send(`Error retrieving warehouse ${err}`)
-    })
-}
+      res.status(400).send(`Error retrieving warehouse ${err}`);
+    });
+};
 
 const getStock = (req, res) => {
-    knex('inventories')
+  knex("inventories")
     .where({ warehouse_id: req.params.id })
     .then((data) => {
-        res.status(200).json(data);
+      res.status(200).json(data);
     })
     .catch((err) => {
-        res.status(400).send(`Error retrieving warehouse inventory ${err}`)
-    })
-}
-
+      res.status(400).send(`Error retrieving warehouse inventory ${err}`);
+    });
+};
 
 module.exports = {
-    getAll,
-    getOne,
-    getStock
-}
+  getAll,
+  getOne,
+  getStock,
+};
