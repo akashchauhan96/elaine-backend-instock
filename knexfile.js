a1a7bf92bba431c57291de60038922db24c345b3
Akash Chauhan <akash.sc96@gmail.com>
2023-05-15 11:59:36 -0400

    Setup knexfile.js parameters for railway app db connection

--- knexfile.js
+++ knexfile.js
@@ -1,16 +1,10 @@
 require("dotenv").config();
 
 // Deconstruct variables from .env file
-const { DB_NAME, DB_USER, DB_PASSWORD } = process.env;
+const urlDB = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.PORT}/${process.env.DB_NAME}`;
 
 // Connect database with Knex
 module.exports = {
   client: "mysql2",
-  connection: {
-    host: "127.0.0.1",
-    user: DB_USER,
-    password: DB_PASSWORD,
-    database: DB_NAME,
-    charset: "utf8",
-  },
+  connection: urlDB,
 };
